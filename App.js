9c898f4ce77464f6068da8288e913449bc5f687b
Erick <132074706+Erick142002@users.noreply.github.com>
2024-02-28 20:23:35 +0100

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Login'; // Importa el componente Login
-import FavTeam from './FavTeam'; // Importa el componente FavTeam
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import Login from './Login';
+import FavTeam from './FavTeam';
 import HomeLaLiga from './HomeLaLiga';
-import HomeBundesliga from './HomeBundesliga'; 
+import HomeBundesliga from './HomeBundesliga';
 import HomeLeague1 from './HomeLeague1';
 import HomePremierLeague from './HomePremierLeague';
 import HomeSerieA from './HomeSerieA';
 
 function App() {
+  const navigate = useNavigate(); // Obtener la función navigate
+
+  // Redirigir a la página de inicio de sesión
+  React.useEffect(() => {
+    navigate('/login');
+  }, [navigate]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} /> {/* Ruta para la página de inicio de sesión */}
-        <Route path="/favteam" element={<FavTeam />} /> {/* Ruta para la selección de equipo favorito */}
-        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/favteam" element={<FavTeam />} />
         <Route path="/homeLaLiga" element={<HomeLaLiga />} />
         <Route path="/homeBundesliga" element={<HomeBundesliga />} />
         <Route path="/homeLeague1" element={<HomeLeague1 />} />
@@ -26,5 +32,3 @@ function App() {
 }
 
 export default App;
-
-
